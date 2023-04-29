4b22391a302b611952a10dd16aff14eff2a9dda3
Cocoa <i@uwucocoa.moe>
2023-04-29 23:52:39 +0100

    init

---
+++ lib/hnsw_elixir.ex
@@ -0,0 +1,18 @@
+defmodule HnswElixir do
+  @moduledoc """
+  Documentation for `HnswElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HnswElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hnsw_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule HnswElixirTest do
+  use ExUnit.Case
+  doctest HnswElixir
+
+  test "greets the world" do
+    assert HnswElixir.hello() == :world
+  end
+end
