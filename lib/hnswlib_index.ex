33b53278c70b9d806145ca0f0b64d7065da52eda
Cocoa <i@uwucocoa.moe>
2023-04-30 10:13:21 +0100

    [test] updated tests for `HNSWLib.Index.new/{3,4}`

--- lib/hnswlib_index.ex
+++ lib/hnswlib_index.ex
@@ -43,30 +43,51 @@ defmodule HNSWLib.Index do
   def new(space, dim, max_elements, opts \\ [])
       when (space == :l2 or space == :ip or space == :cosine) and is_integer(dim) and dim >= 0 and
              is_integer(max_elements) and max_elements >= 0 do
-    m = opts[:m] || 16
-    ef_construction = opts[:ef_construction] || 200
-    random_seed = opts[:random_seed] || 100
-    allow_replace_deleted = opts[:allow_replace_deleted] || false
-
-    case HNSWLib.Nif.new(
-           space,
-           dim,
-           max_elements,
-           m,
-           ef_construction,
-           random_seed,
-           allow_replace_deleted
-         ) do
-      {:ok, ref} when is_reference(ref) ->
-        {:ok,
-         %T{
-           space: space,
-           dim: dim,
-           reference: ref
-         }}
-
+    with {:ok, m} <- get_keyword(opts, :m, :non_neg_integer, 16),
+         {:ok, ef_construction} <- get_keyword(opts, :ef_construction, :non_neg_integer, 200),
+         {:ok, random_seed} <- get_keyword(opts, :random_seed, :non_neg_integer, 100),
+         {:ok, allow_replace_deleted} <-
+           get_keyword(opts, :allow_replace_deleted, :boolean, false),
+         {:ok, ref} <-
+           HNSWLib.Nif.new(
+             space,
+             dim,
+             max_elements,
+             m,
+             ef_construction,
+             random_seed,
+             allow_replace_deleted
+           ) do
+      {:ok,
+       %T{
+         space: space,
+         dim: dim,
+         reference: ref
+       }}
+    else
       {:error, reason} ->
         {:error, reason}
     end
+  end
+
+  defp get_keyword(opts, key, type, default) do
+    get_keyword(key, opts[key] || default, type)
+  end
+
+  defp get_keyword(_key, val, :non_neg_integer) when is_integer(val) and val >= 0 do
+    {:ok, val}
+  end
+
+  defp get_keyword(key, val, :non_neg_integer) do
+    {:error,
+     "expect keyword parameter `#{inspect(key)}` to be a non-negative integer, got `#{inspect(val)}`"}
+  end
+
+  defp get_keyword(_key, val, :boolean) when is_boolean(val) do
+    {:ok, val}
+  end
+
+  defp get_keyword(key, val, :boolean) do
+    {:error, "expect keyword parameter `#{inspect(key)}` to be a boolean, got `#{inspect(val)}`"}
   end
 end
--- test/hnswlib_index_test.exs
+++ test/hnswlib_index_test.exs
@@ -69,4 +69,91 @@ defmodule HNSWLib.Index.Test do
     assert space == index.space
     assert dim == index.dim
   end
+
+  test "HNSWLib.Index.new/3 with non-default keyword parameters" do
+    space = :ip
+    dim = 8
+    max_elements = 200
+
+    m = 200
+    ef_construction = 400
+    random_seed = 42
+    allow_replace_deleted = true
+
+    {:ok, index} =
+      HNSWLib.Index.new(space, dim, max_elements,
+        m: m,
+        ef_construction: ef_construction,
+        random_seed: random_seed,
+        allow_replace_deleted: allow_replace_deleted
+      )
+
+    assert is_reference(index.reference)
+    assert space == index.space
+    assert dim == index.dim
+
+    dim = 12
+
+    {:ok, index} =
+      HNSWLib.Index.new(space, dim, max_elements,
+        m: m,
+        ef_construction: ef_construction,
+        random_seed: random_seed,
+        allow_replace_deleted: allow_replace_deleted
+      )
+
+    assert is_reference(index.reference)
+    assert space == index.space
+    assert dim == index.dim
+  end
+
+  test "HNSWLib.Index.new/3 with invalid keyword parameter m" do
+    space = :ip
+    dim = 8
+    max_elements = 200
+
+    m = -1
+
+    assert {:error,
+            "expect keyword parameter `:m` to be a non-negative integer, got `#{inspect(m)}`"} ==
+             HNSWLib.Index.new(space, dim, max_elements, m: m)
+  end
+
+  test "HNSWLib.Index.new/3 with invalid keyword parameter ef_construction" do
+    space = :ip
+    dim = 8
+    max_elements = 200
+
+    ef_construction = -1
+
+    assert {:error,
+            "expect keyword parameter `:ef_construction` to be a non-negative integer, got `#{inspect(ef_construction)}`"} ==
+             HNSWLib.Index.new(space, dim, max_elements, ef_construction: ef_construction)
+  end
+
+  test "HNSWLib.Index.new/3 with invalid keyword parameter random_seed" do
+    space = :ip
+    dim = 8
+    max_elements = 200
+
+    random_seed = -1
+
+    assert {:error,
+            "expect keyword parameter `:random_seed` to be a non-negative integer, got `#{inspect(random_seed)}`"} ==
+             HNSWLib.Index.new(space, dim, max_elements, random_seed: random_seed)
+  end
+
+  test "HNSWLib.Index.new/3 with invalid keyword parameter allow_replace_deleted" do
+    space = :ip
+    dim = 8
+    max_elements = 200
+
+    allow_replace_deleted = -1
+
+    assert {:error,
+            "expect keyword parameter `:allow_replace_deleted` to be a boolean, got `#{inspect(allow_replace_deleted)}`"} ==
+             HNSWLib.Index.new(space, dim, max_elements,
+               allow_replace_deleted: allow_replace_deleted
+             )
+  end
 end
