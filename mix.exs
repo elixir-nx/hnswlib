e8621fd2a0f45fb1fc3da56fdf1282c269ec25d2
José Valim <jose.valim@dashbit.co>
2023-09-14 08:43:07 +0200

    Add precompilation for Erlang/OTP 26

--- mix.exs
+++ mix.exs
@@ -16,7 +16,8 @@ defmodule HNSWLib.MixProject do
       compilers: [:elixir_make] ++ Mix.compilers(),
       make_precompiler: {:nif, CCPrecompiler},
       make_precompiler_url: "#{@github_url}/releases/download/v#{@version}/@{artefact_filename}",
-      make_precompiler_filename: "hnswlib_nif"
+      make_precompiler_filename: "hnswlib_nif",
+      make_precompiler_nif_versions: [versions: ["2.16", "2.17"]]
     ]
   end
 
