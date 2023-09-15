d63ec199fb34d86c857d054f64238dcb7c2fcb97
Cocoa <i@uwucocoa.moe>
2023-09-16 04:54:18 +0800

    [ci] split into precompile-manylinux.yml

--- .github/do-build-manylinux2014.sh
+++ .github/do-build-manylinux2014.sh
@@ -5,27 +5,23 @@ set -x
 MIX_ENV=$1
 OTP_VERSION=$2
 ELIXIR_VERSION=$3
+OPENSSL_VERSION=$4
+ARCH=$5
 
-OPENSSL_VERSION="3.1.1"
-ASDF_VERSION="v0.12.0"
+OTP_MAJOR_VERSION=$(cut -d "." -f 1 <<< "$OTP_VERSION")
+OPENSSL_VERSION=${OPENSSL_VERSION:-3.1.1}
+PERFIX_DIR="/openssl-${ARCH}"
+OPENSSL_ARCHIVE="openssl-${ARCH}.tar.gz"
 
-yum install -y openssl-devel ncurses-devel perl-IPC-Cmd python3 && \
+yum install -y ncurses-devel && \
     cd / && \
-    curl -fSL https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz -o openssl-${OPENSSL_VERSION}.tar.gz && \
-    tar xf openssl-${OPENSSL_VERSION}.tar.gz && \
-    cd openssl-${OPENSSL_VERSION} && \
-    ./Configure && make && \
-    make DESTDIR="/openssl-${OPENSSL_VERSION}/openssl-build" install &&
-    cd / && \
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch ${ASDF_VERSION} && \
-    . "/root/.asdf/asdf.sh" && \
-    export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=/openssl-${OPENSSL_VERSION}/openssl-build/usr/local" && \
-    asdf plugin add erlang && \
-    asdf plugin add elixir && \
-    asdf install erlang ${OTP_VERSION} && \
-    asdf install elixir ${ELIXIR_VERSION} && \
-    asdf global erlang ${OTP_VERSION} && \
-    asdf global elixir ${ELIXIR_VERSION} && \
+    curl -fSL "https://github.com/cocoa-xu/elixir_make-manylinux-openssl-precompiled/releases/download/v${OPENSSL_VERSION}/${OPENSSL_ARCHIVE}" -o "${OPENSSL_ARCHIVE}" && \
+    tar -xf "${OPENSSL_ARCHIVE}" && \
+    mkdir -p "elixir-${ELIXIR_VERSION}" && \
+    cd "elixir-${ELIXIR_VERSION}" && \
+    curl -fSL "https://github.com/elixir-lang/elixir/releases/download/${ELIXIR_VERSION}/elixir-otp-${OTP_MAJOR_VERSION}.zip" -o "elixir-otp-${OTP_MAJOR_VERSION}.zip" && \
+    unzip "elixir-otp-${OTP_MAJOR_VERSION}.zip" && \
+    export PATH="/elixir-${ELIXIR_VERSION}/bin:${PATH}" && \
     export CMAKE_HNSWLIB_OPTIONS="-D CMAKE_C_FLAGS=\"-static-libgcc -static-libstdc++\" -D CMAKE_CXX_FLAGS=\"-static-libgcc -static-libstdc++\"" && \
     cd /work && \
     mix deps.get
@@ -35,3 +31,4 @@ cd /work
 export MIX_ENV="${MIX_ENV}"
 
 mix compile
+mix elixir_make.precompile
--- .github/script-to-build-manylinux2014.sh
+++ .github/script-to-build-manylinux2014.sh
@@ -5,9 +5,11 @@ set -x
 MIX_ENV=$1
 OTP_VERSION=$2
 ELIXIR_VERSION=$3
+OPENSSL_VERSION=$4
+ARCH=$5
 
-sudo docker run --privileged --network=host --rm -v `pwd`:/work quay.io/pypa/manylinux2014_x86_64:latest \
-    sh -c "chmod a+x /work/do-build-manylinux2014.sh && /work/do-build-manylinux2014.sh ${MIX_ENV} ${OTP_VERSION} ${ELIXIR_VERSION}"
+sudo docker run --privileged --network=host --rm -v `pwd`:/work "quay.io/pypa/manylinux2014_$ARCH:latest" \
+    sh -c "chmod a+x /work/do-build-manylinux2014.sh && /work/do-build-manylinux2014.sh ${MIX_ENV} ${OTP_VERSION} ${ELIXIR_VERSION} ${OPENSSL_VERSION} ${ARCH}"
 
 if [ -d "`pwd`/_build" ]; then
   sudo chmod -R a+wr `pwd`/_build ;
