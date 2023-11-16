c5dc51630405d5638769e8428c753dcb6f3e188d
Cocoa <i@uwucocoa.moe>
2023-11-16 22:06:27 +0800

    updated manylinux build

--- .github/script-to-build-manylinux2014.sh
+++ .github/script-to-build-manylinux2014.sh
@@ -11,6 +11,6 @@ ARCH=$5
 sudo docker run --privileged --network=host --rm -v `pwd`:/work "quay.io/pypa/manylinux2014_$ARCH:latest" \
     sh -c "chmod a+x /work/do-build-manylinux2014.sh && /work/do-build-manylinux2014.sh ${MIX_ENV} ${OTP_VERSION} ${ELIXIR_VERSION} ${OPENSSL_VERSION} ${ARCH}"
 
-if [ -d "`pwd`/_build" ]; then
-  sudo chmod -R a+wr `pwd`/_build ;
+if [ -d "`pwd`/cache" ]; then
+  sudo chmod -R a+wr `pwd`/cache ;
 fi
