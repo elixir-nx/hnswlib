4678c5e370ed117594d06ed85a5a15bdb01d0947
Cocoa <i@uwucocoa.moe>
2024-02-29 21:55:11 +0800

    v0.1.5

--- .github/do-build-manylinux2014.sh
+++ .github/do-build-manylinux2014.sh
@@ -10,18 +10,21 @@ ARCH=$5
 HNSWLIB_CI_PRECOMPILE=$6
 
 OTP_MAJOR_VERSION=$(cut -d "." -f 1 <<< "$OTP_VERSION")
-OPENSSL_VERSION=${OPENSSL_VERSION:-3.1.1}
+OPENSSL_VERSION=${OPENSSL_VERSION:-3.2.1}
 PERFIX_DIR="/openssl-${ARCH}"
-OPENSSL_ARCHIVE="openssl-${ARCH}.tar.gz"
-OTP_ARCHIVE="otp-${ARCH}.tar.gz"
+OPENSSL_ARCHIVE="openssl-${ARCH}-linux-gnu.tar.gz"
+OTP_ARCHIVE="otp-${ARCH}-linux-gnu.tar.gz"
 
 yum install -y openssl-devel ncurses-devel && \
     cd / && \
-    curl -fSL "https://github.com/cocoa-xu/elixir_make-manylinux-openssl-precompiled/releases/download/v${OPENSSL_VERSION}/${OPENSSL_ARCHIVE}" -o "${OPENSSL_ARCHIVE}" && \
-    tar -xf "${OPENSSL_ARCHIVE}" && \
-    curl -fSL "https://github.com/cocoa-xu/elixir_make-manylinux-otp-precompiled/releases/download/v${OTP_VERSION}/${OTP_ARCHIVE}" -o "${OTP_ARCHIVE}" && \
-    tar -xf "${OTP_ARCHIVE}" && \
-    export PATH="/${OTP_VERSION}/bin:${PATH}" && \
+    curl -fSL "https://github.com/cocoa-xu/openssl-build/releases/download/v${OPENSSL_VERSION}/${OPENSSL_ARCHIVE}" -o "${OPENSSL_ARCHIVE}" && \
+    mkdir -p "${PERFIX_DIR}" && \
+    tar -xf "${OPENSSL_ARCHIVE}" -C "${PERFIX_DIR}" && \
+    curl -fSL "https://github.com/cocoa-xu/otp-build/releases/download/v${OTP_VERSION}/${OTP_ARCHIVE}" -o "${OTP_ARCHIVE}" && \
+    mkdir -p "otp" && \
+    tar -xf "${OTP_ARCHIVE}" -C "otp" && \
+    export PATH="/otp/usr/local/bin:${PATH}" && \
+    export ERL_ROOTDIR="/otp/usr/local/lib/erlang" && \
     mkdir -p "elixir-${ELIXIR_VERSION}" && \
     cd "elixir-${ELIXIR_VERSION}" && \
     curl -fSL "https://github.com/elixir-lang/elixir/releases/download/${ELIXIR_VERSION}/elixir-otp-${OTP_MAJOR_VERSION}.zip" -o "elixir-otp-${OTP_MAJOR_VERSION}.zip" && \
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule HNSWLib.MixProject do
   use Mix.Project
 
-  @version "0.1.4"
+  @version "0.1.5"
   @github_url "https://github.com/elixir-nx/hnswlib"
 
   def project do
